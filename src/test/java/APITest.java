145355eac759b0d6d4c6201ffba05fc726d87b72
qinghanz <qinghan.zhang@mail.mcgill.ca>
2023-02-09 15:32:34 -0500

    todos POST test

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -1,12 +1,14 @@
 import java.io.IOException;
+import java.util.HashMap;
 
+import okhttp3.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
+import org.json.simple.parser.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class APITest {
     OkHttpClient client = new OkHttpClient();
@@ -26,11 +28,55 @@ public class APITest {
     public void todoGetAllRequest() throws Exception {
         Request request = new Request.Builder()
                 .url("http://localhost:4567/todos")
+                .get()
                 .build();
 
         Response response = client.newCall(request).execute();
-
+        assertNotNull(response.body().string());
         assertEquals(200, response.code());
     }
 
+    @Test
+    public void todoHeadRequest() throws Exception {
+        Request request = new Request.Builder()
+                .url("http://localhost:4567/todos")
+                .head()
+                .build();
+
+        Response response = client.newCall(request).execute();
+        //System.out.println(response.body().string());
+        assertEquals(200, response.code());
+    }
+
+    @Test
+    public void todoPostRequest() throws Exception {
+        String title = "Random Project";
+        boolean doneStatus = false;
+        String description =  "This project must be completed by monday";
+
+        JSONObject obj = new JSONObject();
+
+        obj.put("title", "Random Project");
+        obj.put("doneStatus", doneStatus);
+        obj.put("description", "This project must be completed by monday");
+
+        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), obj.toString());
+
+        Request request = new Request.Builder()
+                .url("http://localhost:4567/todos")
+                .post(body)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String responseBody = response.body().string();
+
+            JSONParser parser = new JSONParser();
+            JSONObject responseJson = (JSONObject) parser.parse(responseBody);
+
+            String responseTitle = (String) responseJson.get("title");
+            String responseDescription = (String) responseJson.get("description");
+
+            assertEquals(title, responseTitle);
+            assertEquals(description, responseDescription);
+        }
 }
