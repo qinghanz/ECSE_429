54c4799e09ebc4caa4c91c2197027c550df2abfc
Neel Faucher <faucherneel@gmail.com>
2023-02-11 15:44:37 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- src/test/java/APITEST_PROJECTS.java
+++ src/test/java/APITEST_PROJECTS.java
@@ -16,7 +16,7 @@ public class APITEST_PROJECTS {
     OkHttpClient client = new OkHttpClient();
 
     @Test
-    public void todoGetAllProjects() throws Exception {
+    public void getAllProjects() throws Exception {
         Request request = new Request.Builder()
                 .url("http://localhost:4567/projects")
                 .get()
@@ -74,10 +74,12 @@ public class APITEST_PROJECTS {
         assertEquals(description, responseDescription);
     }
 
+
+
     @Test
-    public void todoGetIDRequest() throws Exception {
+    public void projectsGetIDRequest() throws Exception {
         Request request = new Request.Builder()
-                .url("http://localhost:4567/todos/2")
+                .url("http://localhost:4567/projects/1")
                 .build();
 
         Response response = client.newCall(request).execute();
@@ -89,20 +91,34 @@ public class APITEST_PROJECTS {
         JSONObject responseJson = (JSONObject) parser.parse(responseBody);
 
         JSONObject jsonObject = new JSONObject(responseJson);
-        JSONArray todos = (JSONArray) jsonObject.get("todos");
+        JSONArray projects = (JSONArray) jsonObject.get("projects");
 
-        for (Object todoObject : todos) {
-            JSONObject todo = (JSONObject) todoObject;
-            String title = (String) todo.get("title");
-            String description = (String) todo.get("description");
-            assertEquals("file paperwork", title);
+        for (Object projectObject : projects) {
+            JSONObject project = (JSONObject) projectObject;
+            String title = (String) project.get("title");
+            String description = (String) project.get("description");
+
+            // should i be using the Booleans?
+            assertEquals("Testing Changed Project #2", title);
             assertEquals("", description);
         }
     }
 
     @Test
-    public void todoPostIDRequest() throws Exception {
-        String title = "Random Changed Project";
+    public void projectHead_IDRequest() throws Exception {
+        Request request = new Request.Builder()
+                .url("http://localhost:4567/projects/1")
+                .head()
+                .build();
+
+        Response response = client.newCall(request).execute();
+        //System.out.println(response.body().string());
+        assertEquals(200, response.code());
+    }
+
+    @Test
+    public void projectPostIDRequest() throws Exception {
+        String title = "Testing Changed Project";
 
         JSONObject obj = new JSONObject();
 
@@ -111,7 +127,7 @@ public class APITEST_PROJECTS {
         RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), obj.toString());
 
         Request request = new Request.Builder()
-                .url("http://localhost:4567/todos/1")
+                .url("http://localhost:4567/projects/1")
                 .post(body)
                 .build();
 
@@ -125,9 +141,11 @@ public class APITEST_PROJECTS {
         assertEquals(title, responseTitle);
     }
 
+
+
     @Test
-    public void todoPutIDRequest() throws Exception {
-        String title = "Random Changed Project Again";
+    public void projectPutIDRequest() throws Exception {
+        String title = "Testing Changed Project #2";
 
         JSONObject obj = new JSONObject();
 
@@ -136,7 +154,7 @@ public class APITEST_PROJECTS {
         RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), obj.toString());
 
         Request request = new Request.Builder()
-                .url("http://localhost:4567/todos/1")
+                .url("http://localhost:4567/projects/1")
                 .put(body)
                 .build();
 
@@ -150,36 +168,97 @@ public class APITEST_PROJECTS {
         assertEquals(title, responseTitle);
     }
 
+
+    // Creating project to then delete
     @Test
-    public void todoDeleteRequest() throws Exception {
-        String title = "Random Project to Delete";
-        boolean doneStatus = false;
-        String description = "This project must be deleted";
+    public void projectsPostRequest_toDelete() throws Exception {
+        String title = "Project to delete";
+        boolean completed = true;
+        boolean active = true;
+        String description = "this will be deleted";
 
         JSONObject obj = new JSONObject();
         obj.put("title", title);
-        obj.put("doneStatus", doneStatus);
+        obj.put("completed", completed);
+        obj.put("active", active);
         obj.put("description", description);
 
         RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), obj.toString());
 
         Request request = new Request.Builder()
-                .url("http://localhost:4567/todos")
+                .url("http://localhost:4567/projects")
                 .post(body)
                 .build();
 
         Response response = client.newCall(request).execute();
-        assertEquals(201, response.code());
         String responseBody = response.body().string();
+
         JSONParser parser = new JSONParser();
         JSONObject responseJson = (JSONObject) parser.parse(responseBody);
-        String todoId = (String) responseJson.get("id");
+
+        System.out.println(responseBody);
+
+        String responseTitle = (String) responseJson.get("title");
+        String responseDescription = (String) responseJson.get("description");
+
+        assertEquals(title, responseTitle);
+        assertEquals(description, responseDescription);
+    }
+
+    // deleting the previously created project
+    @Test
+    public void todoDeleteRequest() throws Exception {
 
         Request deleteRequest = new Request.Builder()
-                .url("http://localhost:4567/todos/" + todoId)
+                .url("http://localhost:4567/projects/8")
                 .delete()
                 .build();
         Response deleteResponse = client.newCall(deleteRequest).execute();
         assertEquals(200, deleteResponse.code());
     }
+
+    @Test
+    public void projectsGetID_TaskRequest() throws Exception {
+        Request request = new Request.Builder()
+                .url("http://localhost:4567/projects/1/tasks")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        assertEquals(200, response.code());
+
+        String responseBody = response.body().string();
+
+        JSONParser parser = new JSONParser();
+        JSONObject responseJson = (JSONObject) parser.parse(responseBody);
+
+        JSONObject jsonObject = new JSONObject(responseJson);
+        JSONArray projects = (JSONArray) jsonObject.get("projects");
+
+        for (Object projectObject : projects) {
+            JSONObject project = (JSONObject) projectObject;
+            String title = (String) project.get("title");
+            String description = (String) project.get("description");
+
+            assertEquals("Testing Changed Project #2", title);
+            assertEquals("", description);
+        }
+    }
+
+    @Test
+    public void projectHeadRequest_ID() throws Exception {
+        Request request = new Request.Builder()
+                .url("http://localhost:4567/projects/1/tasks")
+                .head()
+                .build();
+
+        Response response = client.newCall(request).execute();
+        //System.out.println(response.body().string());
+        assertEquals(200, response.code());
+    }
+
+
+
+
+    // ------------------------------------------------------------------------------------------------------------------------------------------------
+
 }
