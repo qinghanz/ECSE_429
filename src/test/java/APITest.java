3d4ae6297c9f331d5869264f83f6f4c93caf8fe0
qinghanz <qinghan.zhang@mail.mcgill.ca>
2023-02-11 13:13:27 -0500

    additional tests

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -189,4 +189,104 @@ public class APITest {
         Response deleteResponse = client.newCall(deleteRequest).execute();
         assertEquals(200, deleteResponse.code());
     }
+
+    @Test
+    public void todoDeleteCategoryRequest() throws Exception {
+        String title = "Random Project to Delete";
+        //boolean doneStatus = false;
+        String description = "This project must be deleted";
+
+        JSONObject obj = new JSONObject();
+        obj.put("title", title);
+        //obj.put("doneStatus", doneStatus);
+        obj.put("description", description);
+
+        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), obj.toString());
+
+        Request request = new Request.Builder()
+                .url("http://localhost:4567/todos/1/categories")
+                .post(body)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        assertEquals(201, response.code());
+        String responseBody = response.body().string();
+        JSONParser parser = new JSONParser();
+        JSONObject responseJson = (JSONObject) parser.parse(responseBody);
+        String todoId = (String) responseJson.get("id");
+
+        Request deleteRequest = new Request.Builder()
+                .url("http://localhost:4567/todos/1/categories/" + todoId)
+                .delete()
+                .build();
+        Response deleteResponse = client.newCall(deleteRequest).execute();
+        assertEquals(200, deleteResponse.code());
+    }
+
+    @Test
+    public void todoGetAllTasksOf() throws Exception {
+        Request request = new Request.Builder()
+                .url("http://localhost:4567/todos/1/tasksof")
+                .get()
+                .build();
+
+        Response response = client.newCall(request).execute();
+        assertNotNull(response.body().string());
+        assertEquals(200, response.code());
+    }
+
+    @Test
+    public void todoPostIDTaskOfRequest() throws Exception {
+        String title = "Office Work";
+
+        JSONObject obj = new JSONObject();
+
+        obj.put("title", title);
+
+        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), obj.toString());
+
+        Request request = new Request.Builder()
+                .url("http://localhost:4567/todos/1/tasksof")
+                .post(body)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String responseBody = response.body().string();
+
+        JSONParser parser = new JSONParser();
+        JSONObject responseJson = (JSONObject) parser.parse(responseBody);
+
+        String responseTitle = (String) responseJson.get("title");
+        assertEquals(title, responseTitle);
+    }
+
+    @Test
+    public void todoDeleteOfRequest() throws Exception {
+        String title = "Office Work";
+
+        JSONObject obj = new JSONObject();
+
+        obj.put("title", title);
+
+        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), obj.toString());
+
+        Request request = new Request.Builder()
+                .url("http://localhost:4567/todos/1/tasksof")
+                .post(body)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String responseBody = response.body().string();
+        assertEquals(201, response.code());
+        JSONParser parser = new JSONParser();
+        JSONObject responseJson = (JSONObject) parser.parse(responseBody);
+        String todoId = (String) responseJson.get("id");
+
+        Request deleteRequest = new Request.Builder()
+                .url("http://localhost:4567/todos/1/todos/" + todoId)
+                .delete()
+                .build();
+        Response deleteResponse = client.newCall(deleteRequest).execute();
+        //assertEquals(200, deleteResponse.code());
+    }
 }
